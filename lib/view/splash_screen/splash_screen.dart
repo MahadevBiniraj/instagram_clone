74e84e31028fb87d3bbd02c841f58c96975f5685
MahadevBiniraj <mahadevbiniraj6@gmail.com>
2024-02-26 13:27:22 +0530

    first commit

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashScreeen extends StatelessWidget {
+  const SplashScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image(image: AssetImage("assets/images/Icon.png"))),
+    );
+  }
+}
